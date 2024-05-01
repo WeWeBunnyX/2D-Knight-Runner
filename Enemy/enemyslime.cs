38792e724a30eebfd89709975afeb97050055666
Hashir Asad <hashirbinasad@gmail.com>
2024-05-01 22:38:38 +0500

    Using raycasts collision detection for enemy slime

--- Enemy/enemyslime.cs
+++ Enemy/enemyslime.cs
@@ -3,14 +3,25 @@ using System;
 
 public partial class enemyslime : Node2D
 {
-	// Called when the node enters the scene tree for the first time.
+
+	[Export]                                   
+    private NodePath _RaycastPath;  
+
+	[Export] 
+	private NodePath _RaycastPath2;                
+    private RayCast2D _Raycast;  
+	private RayCast2D _Raycast2;    
+
+	
 	public override void _Ready()
 	{
+		_Raycast = GetNode<RayCast2D>(_RaycastPath);  
+		_Raycast2 = GetNode<RayCast2D>(_RaycastPath2);  
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	    Position += new Vector2(-1, 0);                        // this actually moves the sprite           
+	    Position += new Vector2(-1, 0);                              
 	}
 }

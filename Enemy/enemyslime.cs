f047b761401753fd50dbcd4c6f7ba1241ac145fb
Hashir Asad <hashirbinasad@gmail.com>
2024-05-01 22:44:43 +0500

    for now

--- Enemy/enemyslime.cs
+++ Enemy/enemyslime.cs
@@ -5,23 +5,31 @@ public partial class enemyslime : Node2D
 {
 
 	[Export]                                   
-    private NodePath _RaycastPath;  
+    private NodePath _RaycastPathLeft;  
 
 	[Export] 
-	private NodePath _RaycastPath2;                
-    private RayCast2D _Raycast;  
-	private RayCast2D _Raycast2;    
+	private NodePath _RaycastPathRight;                
+    private RayCast2D _RaycastLeft;  
+	private RayCast2D _RaycastRight;    
 
 	
 	public override void _Ready()
 	{
-		_Raycast = GetNode<RayCast2D>(_RaycastPath);  
-		_Raycast2 = GetNode<RayCast2D>(_RaycastPath2);  
+		_RaycastLeft = GetNode<RayCast2D>(_RaycastPathLeft);  
+		_RaycastRight = GetNode<RayCast2D>(_RaycastPathRight);  
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-	    Position += new Vector2(-1, 0);                              
+
+
+	Position += new Vector2(-1, 0); 
+	
+	if (_RaycastLeft.IsColliding())
+	    Position += new Vector2(-1, 0);  
+
+	if (_RaycastRight.IsColliding())
+		Position += new Vector2(1, 0);                            
 	}
 }

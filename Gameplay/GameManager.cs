0f2274d3970593cac98d13304beb2d7c540af56f
Hashir Asad <hashirbinasad@gmail.com>
2024-05-03 23:57:11 +0500

    c

--- Gameplay/GameManager.cs
+++ Gameplay/GameManager.cs
@@ -3,14 +3,23 @@ using System;
 
 public partial class GameManager : Node
 {
+	[Export]
+	public NodePath _ScorePath;
 
+	private Label _ScoreLabel;
 
+	public override void _Ready()
+	{
+		_ScoreLabel = GetNode<Label>(_ScorePath);
+		
+	}
 	public int score = 0;
 
 	public void AddScore()
 	{
 		score += 1;
 		Console.WriteLine("Score: " + score);
+		_ScoreLabel.Text = "Score: " + score;
 	}
 
 

5fff9c4b416d91eab0ce61fbcd3be83350b4ae2c
Hashir Asad <hashirbinasad@gmail.com>
2024-05-04 14:28:06 +0500

    Major Problem Solved
    We encountered an issue with multiple Coin scenes added to the main GamePlay Node. The problem arose from setting duplicate paths for the GameManager node for each coin. To resolve this, we took the following steps:
    
    We removed the coins from the GamePlay Node (Coin group) to address the duplication.
    
    We then focused on the individual Coin scene. However, we faced difficulty in selecting the GameManager node in the path setting, as it exists outside the current Coin scene.
    
    To overcome this obstacle, we manually provided the full path to the GameManager node, starting from the root of the scene tree. In our case, the path looked like this: /root/Gameplay/GameManager.
    In this setup:
    
    /root denotes the root of the scene tree.
    
    Gameplay acts as the parent node of the GameManager node.
    
    GameManager represents the specific node we're referencing.
    
    By ensuring the correctness of this path, we successfully accessed the GameManager node from within the Coin scene. This approach can be applied to any scenario where you need to interact with nodes outside the current scene.

--- Pickups/coin.cs
+++ Pickups/coin.cs
@@ -6,19 +6,26 @@ public partial class coin : Area2D
 	[Export]
 	public NodePath _GameManagerPath;
 
+	[Export]
+
+	public NodePath _AnimationPlayerPath;
+
+	private AnimationPlayer _AnimationPlayer;
+
 	private GameManager _GameManager;
 
 	
 	public override void _Ready()
 	{
 		_GameManager = GetNode<GameManager>(_GameManagerPath);
+		_AnimationPlayer = GetNode<AnimationPlayer>(_AnimationPlayerPath);
 	}
 	
    private void playerBodyEntered(Node2D body)
    {
 	_GameManager.AddScore();
 	Console.WriteLine("Player collected a coin!");
-	QueueFree();
+	_AnimationPlayer.Play("pickup");
 
 
    }

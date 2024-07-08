3604940e3c8871a0772b5226405ee8d4882f6ce9
Hashir Asad <hashirbinasad@gmail.com>
2024-07-08 17:13:37 +0500

    Attack slime script configured, methods to be implemented gradually

---
+++ Enemy/attack_slime.cs
@@ -0,0 +1,44 @@
+using Godot;
+using System;
+
+public partial class attack_slime : Node2D
+{
+	[Export]
+	private NodePath _RaycastPathUp;
+
+    [Export]
+	private NodePath _RaycastPathLeft;
+
+	[Export]
+	private NodePath _RaycastPathRight;
+
+	[Export]
+	private NodePath _AnimatedSpritePath;
+
+    private RayCast2D _RaycastUp;
+    private RayCast2D _RaycastLeft;
+    private RayCast2D _RaycastRight;
+    private AnimatedSprite2D _AnimatedSprite;
+
+
+    public override void _Ready()
+	{
+		_RaycastUp= GetNode<RayCast2D>(_RaycastPathUp);
+		_RaycastLeft= GetNode<RayCast2D>(_RaycastPathLeft);
+		_RaycastRight= GetNode<RayCast2D>(_RaycastPathRight);
+		_AnimatedSprite= GetNode<AnimatedSprite2D>(_AnimatedSpritePath);
+
+
+	}
+
+	private void SlimeAreaEntered(Node2D body)
+	{
+
+
+
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
